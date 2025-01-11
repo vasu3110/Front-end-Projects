57588dc39ba0e14d7b426bfa81fca55de64e87f5
Vasu <vasu22300@gmail.com>
2025-01-12 03:10:41 +0530

    eventListener added on songItemPlay class objects

--- Spotify/script.js
+++ Spotify/script.js
@@ -72,3 +72,25 @@ audioElement.addEventListener('timeupdate',()=>{
 myProgressBar.addEventListener('change',()=>{
     audioElement.currentTime=myProgressBar.value * audioElement.duration/100;
 })
+
+const makeAllPlays=()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((el)=>{
+        el.classList.remove('fa-circle-pause');
+        el.classList.add('fa-circle-play');
+    })
+}
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((el)=>{
+    el.addEventListener('click',(e)=>{
+        console.log(e.target);
+        makeAllPlays();
+        index=parseInt(e.target.id);
+        e.target.classList.remove('fa-circle-play');
+        e.target.classList.add('fa-circle-pause')
+        audioElement.src=`songs/${index+1}.mp3`;
+        audioElement.currentTime=0;
+        audioElement.play();
+        console.log(el.id);
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+    })
+})
